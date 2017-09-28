a8badbac07b641fe7ddf84d817715580d39d0dbb
patwill92 <patrick.rw92@gmail.com>
2017-09-28 10:22:16 -0400

    finalloginhopefully

--- server.js
+++ server.js
@@ -10,8 +10,6 @@ require('./services/passport');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, './client/build')));
-
 mongoose.connect(keys.mongoURI);
 
 app.use(
@@ -25,9 +23,12 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
-});
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 const PORT = process.env.PORT || 5000;
 
