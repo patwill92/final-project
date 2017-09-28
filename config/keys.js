db31d05dfee19d0ec6a1e929e5fed0d8421f60ee
patwill92 <patrick.rw92@gmail.com>
2017-09-27 21:58:23 -0400

    herokuconfig

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,5 @@
-// keys.js - figure out which set of credentials to use
 if(process.env.NODE_ENV === 'production') {
-  // return production keys
   module.exports = require('./prod');
 } else {
-  //return dev keys
   module.exports = require('./dev');
 }
