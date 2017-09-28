86162bafc4db63dfebf206ac38cb98dc61a2f5e8
patwill92 <patrick.rw92@gmail.com>
2017-09-27 22:04:51 -0400

    pleajesus

--- server.js
+++ server.js
@@ -10,13 +10,11 @@ require('./services/passport');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, './client/build')));
-
 mongoose.connect(keys.mongoURI);
 
 app.use(
   cookieSession({
-    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+    maxAge: 30*24*60*60*1000, // 30 days
     keys: [keys.cookieKey]
   })
 );
@@ -25,9 +23,12 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
-});
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 const PORT = process.env.PORT || 5000;
 
