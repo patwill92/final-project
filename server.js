2025e0fa544a4bc38e54d70bd9cee0fa95e4d306
patwill92 <patrick.rw92@gmail.com>
2017-09-27 23:57:27 -0400

    finalchance

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ require('./services/passport');
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, './client/build')));
+
 mongoose.connect(keys.mongoURI);
 
 app.use(
@@ -23,12 +25,9 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
-if(process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  })
-}
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 
