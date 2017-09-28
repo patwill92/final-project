8ea168ef43e5e165a63eec2f3723337b482fe76d
patwill92 <patrick.rw92@gmail.com>
2017-09-27 22:07:53 -0400

    removeserverproxy

--- services/passport.js
+++ services/passport.js
@@ -8,8 +8,7 @@ const User = mongoose.model('users');
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback',
-    proxy: true
+    callbackURL: '/auth/google/callback'
   },
   (accessToken, refreshToken, profile, done) => {
     console.log(profile);
