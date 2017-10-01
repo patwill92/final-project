60ccdc16249756736365fbc87ecb45b5928a8f36
patwill92 <patrick.rw92@gmail.com>
2017-10-01 01:28:40 -0400

    modal needs fixing

--- client/src/components/Authentication/AuthModal.js
+++ client/src/components/Authentication/AuthModal.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react';
 
 const style = {
-  maxWidth: '350px'
+  width: '350px'
 };
 class AuthModal extends Component {
   render() {
     return (
       <div  className='modal fade' id={this.props.id} tabIndex="-1" role="dialog" aria-labelledby={`${this.props.id}Label`} aria-hidden="true">
-        <div style={style} className="modal-dialog" role="document">
+        <div style={style} className="modal-dialog ml-auth mr-auto" role="document">
           <div style={style} className="modal-content">
             <div className="modal-header">
               <h5 className="modal-title" id={`${this.props.id}Label`}>{this.props.title}</h5>
--- config/dev.js
+++ config/dev.js
@@ -2,7 +2,6 @@
 module.exports = {
   googleClientID: '106254118220-t16nev4qsr51pfc237iik5dfmci795nd.apps.googleusercontent.com',
   googleClientSecret: 'eUI7f-q4Z_EqyccRhcFLWgIj',
-  mongoURI: 'mongodb://patwill92:Patwil$4485@ds151163.mlab.com:51163/emaily-dev-patwill92',
-  cookieKey: 'kljsndgkjlnsdkjnfqwttplskrndi',
-  database: 'mongodb://localhost/ghostusers'
+  mongoURI: 'mongodb://localhost/ghostusers',
+  cookieKey: 'kljsndgkjlnsdkjnfqwttplskrndi'
 };
